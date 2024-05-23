7382b7fb069f740b2cc09d8286d14d6da01d753a
Denis69420 <143262999+Denis69420@users.noreply.github.com>
2024-05-23 14:55:58 +0200

    Delete code directory

--- code/user_commands.py
+++
@@ -1,7 +0,0 @@
-import discord
-from discord.ext import commands
-
-def setup(bot: commands.Bot):
-    @bot.command(name='ping')
-    async def ping(ctx: commands.Context):
-        await ctx.send('Pong!')
